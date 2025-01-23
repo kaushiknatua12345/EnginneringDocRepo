48506d2a174496c7a24e0dfc29cbe4b2460b4d21
kaushiknatua <75721842+kaushiknatua12345@users.noreply.github.com>
2025-01-23 17:16:38 +0530

    Update EmployeeDB.cs

--- DotNetOpenAPI/DotNetOpenAPI/Models/EmployeeDB.cs
+++ DotNetOpenAPI/DotNetOpenAPI/Models/EmployeeDB.cs
@@ -1,5 +1,9 @@
 ﻿namespace DotNetOpenAPI.Models
 {
+ /// <summary>
+ /// EmployeeDB class is used to define the structure of the employee details.
+ /// It contains the properties Id, Name, Department, and Email.
+ /// </summary>
     public class EmployeeDB
     {
         public int Id { get; set; }
