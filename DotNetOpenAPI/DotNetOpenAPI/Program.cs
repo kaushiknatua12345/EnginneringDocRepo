e08915dd3bcffe67ed2aa312f98dd3d6acaa3601
kaushiknatua <75721842+kaushiknatua12345@users.noreply.github.com>
2025-01-28 12:33:25 +0530

    Update Program.cs

--- DotNetOpenAPI/DotNetOpenAPI/Program.cs
+++ DotNetOpenAPI/DotNetOpenAPI/Program.cs
@@ -85,13 +85,17 @@ if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
     app.UseSwaggerUI(c =>
     {
          c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+        c.RoutePrefix = string.Empty;
     });
 }
 
 app.UseHttpsRedirection();
 app.UseAuthorization();
 
-app.MapControllers();
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+});
 
 
 
